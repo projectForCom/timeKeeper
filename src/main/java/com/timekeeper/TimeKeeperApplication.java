0bad66da776e2c79df9c80ba724b5192ceee947e
pluselc <user@user-no-MacBook-Pro.local>
2016-10-21 23:52:27 +0900

    feat: first commit

---
+++ src/main/java/com/timekeeper/TimeKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.timekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeKeeperApplication.class, args);
+	}
+}
